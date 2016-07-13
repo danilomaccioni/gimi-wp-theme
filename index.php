f98b289681310f367a5f8166b7d2d9f3954fc746
enricodigi <enricodigi@gmail.com>
2016-07-13 11:37:17 +0200

    add post preview

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php get_header(); ?>
 
 
-	Benvenuto
+	<h2>Benvenuto</h2>
+	
+	<?php if ( have_posts() ): while ( have_posts() ): the_post(); ?>
+	<a href="<?php the_permalink(); ?>"> <?php the_title();?></a>
+	<?php echo get_the_excerpt();?>
+	<?php endwhile; endif; ?>
+	
 	
 <?php get_footer(); ?>
