d30c5415141338205ffc33028ffb6314c6f9d112
enricodigi <enricodigi@gmail.com>
2016-07-13 10:19:52 +0200

    Header and Footer splitted

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
+	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css">
+	<title><?php bloginfo('name'); ?></title>
+	<?php wp_head(); ?>
+	</head>
+<body>
+	<header>
+		<h1><?php bloginfo('name'); ?></h1>
+	</header>
--- index.php
+++ index.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="<?php bloginfo('language'); ?>">
-<head>
-	<meta http-equiv="Content-type" content="text/html; charset=utf-8">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
-	<link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css" type="text/css">
-	<title><?php bloginfo('name'); ?></title>
-	<?php wp_head(); ?>
-	</head>
-<body>
-	<header>
-		<h1><?php bloginfo('name'); ?></h1>
-	</header>
+<?php get_header(); ?>
+
 	<nav>
 		
 		<?php //wp_list_pages(array('title_li' => null )); ?>
@@ -28,10 +17,4 @@
 		<?php endif; ?>
 	</div>
 	
-	<footer>
-		CC G.I.M.I. 2004
-	</footer>
-	<?php wp_footer(); ?>
-</body>
-</html>
-
+<?php get_footer(); ?>
