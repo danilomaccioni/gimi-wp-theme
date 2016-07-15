716774d06e391c45b0f1ace5a570be819b59c86e
enricodigi <enricodigi@gmail.com>
2016-07-15 16:09:12 +0200

    add footer widget

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 		<footer class="bottom_footer">
 			CC G.I.M.I. 2004
+			<?php dynamic_sidebar( 'footer_widget_container' ); ?>
 		</footer>
 		<?php wp_footer(); ?>
 		<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/footer.js"></script>
--- functions.php
+++ functions.php
@@ -1,13 +1,17 @@
 <?php
-// Replaces the excerpt "Read More" text by a link
-/*function new_excerpt_more($more) {
-       global $post;
-	return '<a class="moretag" href="'. get_permalink($post->ID) . '"> [ Read the full article... ]</a>';
-}
-add_filter('excerpt_more', 'new_excerpt_more');
 
-if ( function_exists('add_theme_support') ) {
-		add_theme_support('post-thumbnails');
-}*/
+function gimi_widgets_init() {
+
+	register_sidebar( array(
+		'name'          => 'footer widget container',
+		'id'            => 'footer_widget_container',
+		'before_widget' => '<div>',
+		'after_widget'  => '</div>',
+		'before_title'  => '<b class="footer_widget">',
+		'after_title'   => '</b>',
+	) );
+
+}
+add_action( 'widgets_init', 'gimi_widgets_init' );
 
 ?>
