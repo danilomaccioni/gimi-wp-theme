c8897b5af5a1714a4706562d4827cb07f55aba1d
enricodigi <enricodigi@gmail.com>
2016-08-08 22:36:53 +0200

    bug correction and style improvements

--- author.php
+++ author.php
@@ -1,34 +1,34 @@
 <?php get_header(); ?>
 
+<div class="main_container">
 <?php if ( have_posts() ) : ?>
-	<div class="posts_container main_container">
-		<?php get_sidebar('author'); ?>
-		
-		<div class="main_column">
-			<div class="author_title">Post di <?php the_author(); ?></div>
-		
-			<ul class="posts_list author_posts_list">
-				<?php $post_while_counter = 0; global $wp_query;?>
-				<?php while ( have_posts() ) : the_post(); $post_while_counter++ ?>
-				<li>
-					<div class="container_thumbnail">
-						<?php if ( has_post_thumbnail() ) : ?>
-								<a class="post_preview_thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150,150)); ?></a>
-						<?php endif; ?> 
-					</div>
-					<div class="post_preview">
-						<h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
-						<?php print_post_date("post_preview_date");?>
-						<?php /*echo get_the_excerpt();*/   the_content('[ suka... ]'); ?>
-					</div>
-					<?php print_category_and_tag(); ?>
-					<?php print_post_separator($post_while_counter); ?>
-				</li>
-				<?php endwhile; ?>
-			</ul>
-			<?php print_prev_next_links(); ?>
+	<div class="posts_container">
+		<div class="author_title">
+			<h2>Post di <?php the_author(); ?></h2>
 		</div>
+		<ul class="posts_list author_posts_list">
+			<?php $post_while_counter = 0; global $wp_query;?>
+			<?php while ( have_posts() ) : the_post(); $post_while_counter++ ?>
+			<li>
+				<div class="container_thumbnail">
+					<?php if ( has_post_thumbnail() ) : ?>
+							<a class="post_preview_thumbnail" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(array(150,150)); ?></a>
+					<?php endif; ?> 
+				</div>
+				<div class="post_preview">
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h3>
+					<?php print_post_date("post_preview_date");?>
+					<?php /*echo get_the_excerpt();*/   the_content('[ suka... ]'); ?>
+				</div>
+				<?php print_category_and_tag(); ?>
+				<?php print_post_separator($post_while_counter); ?>
+			</li>
+			<?php endwhile; ?>
+		</ul>
+		<?php print_prev_next_links(); ?>
 	</div>
 <?php endif; ?>
+<?php get_sidebar('author'); ?>
+</div>
 
 <?php get_footer(); ?>
