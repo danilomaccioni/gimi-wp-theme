fdb4f6e81dc94992b54fda380c5e20bb9235871c
enricodigi <enricodigi@gmail.com>
2017-02-20 12:12:19 +0100

    bug correction

--- sidebar-author.php
+++ sidebar-author.php
@@ -8,7 +8,7 @@
 
 		<div class="right_sidebar user_sidebar"> 
 			<?php $user_info = gimi_get_user_info('150'); ?>
-			<div class="box_avatar"> <?php echo esc_html( $user_info['html_tag_avatar'] ); ?> </div>
+			<div class="box_avatar"> <?php echo $user_info['html_tag_avatar']; ?> </div>
 			<div class="box_name"> <?php echo esc_html( $user_info['user_name'] ); ?> </div>
 			<div class="box_roles box_small_text"> <?php echo esc_html( $user_info['roles'] ); ?> </div>
 			<div class="box_registered box_small_text"> <?php echo esc_html( $user_info['registered'] ); ?> </div>
